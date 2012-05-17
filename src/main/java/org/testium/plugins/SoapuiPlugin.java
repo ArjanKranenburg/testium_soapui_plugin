37d15095555e3d9a00ce177e4aada7b453cd8c35
Arjan Kranenburg <Arjan.Kranenburg@gmail.com>
2012-05-17 21:10:02 +0200

    Converted to Maven project

---
+++ src/main/java/org/testium/plugins/SoapuiPlugin.java
@@ -0,0 +1,151 @@
+package org.testium.plugins;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.testium.Testium;
+import org.testium.configuration.ConfigurationException;
+import org.testium.configuration.SoapuiConfiguration;
+import org.testium.configuration.SoapuiConfigurationXmlHandler;
+import org.testium.executor.TestCaseSoapuiExecutor;
+import org.testtoolinterfaces.testresultinterface.TestCaseResultWriter;
+import org.testtoolinterfaces.utils.RunTimeData;
+import org.testtoolinterfaces.utils.Trace;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ * @author Arjan Kranenburg
+ *
+ */
+public final class SoapuiPlugin implements Plugin
+{
+	public SoapuiPlugin()
+	{
+		// nop
+	}
+	
+	public void loadPlugIn(PluginCollection aPluginCollection,
+			RunTimeData aRtData) throws ConfigurationException
+	{
+		File pluginsDir = aRtData.getValueAsFile(Testium.PLUGINSDIR);
+		File soapUILibs = new File( pluginsDir, "SoapUILibs" );
+		try
+		{
+			PluginClassLoader.addDirToClassLoader( soapUILibs );
+		}
+		catch (MalformedURLException e)
+		{
+			throw new ConfigurationException( e );
+		}
+
+		SoapuiConfiguration config = readConfigFiles( aRtData );
+		
+		File log4jFile = config.getLog4jFile();
+		PropertyConfigurator.configure( log4jFile.getAbsolutePath() );
+
+		// Executors
+		TestCaseResultWriter tcResultWriter = aPluginCollection.getTestCaseResultWriter();
+		TestCaseSoapuiExecutor tcSoapuiExecutor = new TestCaseSoapuiExecutor( config, tcResultWriter );
+		aPluginCollection.addTestCaseExecutor( tcSoapuiExecutor );
+	}
+	
+	public SoapuiConfiguration readConfigFiles( RunTimeData anRtData ) throws ConfigurationException
+	{
+		Trace.println(Trace.UTIL);
+
+		File configDir = (File) anRtData.getValue(Testium.CONFIGDIR);
+		File configFile = new File( configDir, "soapUI.xml" );
+		SoapuiConfiguration globalConfig = readConfigFile( configFile, anRtData );
+		
+		File userConfigDir = (File) anRtData.getValue(Testium.USERCONFIGDIR);
+		File userConfigFile = new File( userConfigDir, "soapUI.xml" );
+		SoapuiConfiguration userConfig = new SoapuiConfiguration( null, null, null, null);
+		if ( userConfigFile.exists() )
+		{
+			userConfig = readConfigFile( userConfigFile, anRtData );
+		}
+
+		File project = userConfig.getProject();
+		if (project == null)
+		{
+			project = globalConfig.getProject();
+		}
+
+		String soapInterface = userConfig.getSoapInterface();
+		if (soapInterface == null || soapInterface.isEmpty() )
+		{
+			soapInterface = globalConfig.getSoapInterface();
+		}
+		
+		File soapUILibsDir = userConfig.getSoapUILibsDir();
+		if ( soapUILibsDir == null ||
+			 soapUILibsDir.equals( SoapuiConfigurationXmlHandler.getDefaultSoapUILibsDir(anRtData) ) )
+		{
+			soapUILibsDir = globalConfig.getSoapUILibsDir();
+		}
+		
+		File log4jFile = userConfig.getLog4jFile();
+		if ( log4jFile == null ||
+				log4jFile.equals( SoapuiConfigurationXmlHandler.getDefaultLog4jFile(anRtData) ) )
+		{
+			log4jFile = globalConfig.getLog4jFile();
+		}
+		
+		SoapuiConfiguration config = new SoapuiConfiguration( project,
+		                                                      soapInterface,
+		                                                      soapUILibsDir,
+		                                                      log4jFile );
+
+		return config;
+	}
+	
+	public SoapuiConfiguration readConfigFile( File aConfigFile, RunTimeData aRtData ) throws ConfigurationException
+	{
+		Trace.println(Trace.UTIL, "readConfigFile( " + aConfigFile.getName() + " )", true );
+        // create a parser
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(false);
+        SAXParser saxParser;
+        SoapuiConfigurationXmlHandler handler = null;
+		try
+		{
+			saxParser = spf.newSAXParser();
+			XMLReader xmlReader = saxParser.getXMLReader();
+
+	        // create a handler
+			handler = new SoapuiConfigurationXmlHandler(xmlReader, aRtData);
+
+	        // assign the handler to the parser
+	        xmlReader.setContentHandler(handler);
+
+	        // parse the document
+	        xmlReader.parse( aConfigFile.getAbsolutePath() );
+		}
+		catch (ParserConfigurationException e)
+		{
+			Trace.print(Trace.UTIL, e);
+			throw new ConfigurationException( e );
+		}
+		catch (SAXException e)
+		{
+			Trace.print(Trace.UTIL, e);
+			throw new ConfigurationException( e );
+		}
+		catch (IOException e)
+		{
+			Trace.print(Trace.UTIL, e);
+			throw new ConfigurationException( e );
+		}
+		
+		SoapuiConfiguration configuration = handler.getConfiguration();
+		
+		return configuration;
+	}
+}
---
+++ src/main/java/org/testtoolinterfaces/testsuite/SoapUI_TestCase.java
@@ -0,0 +1,41 @@
+package org.testtoolinterfaces.testsuite;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+
+
+/**
+ * A Wrapper for SoapUI Test Cases
+ * 
+ * @author Arjan Kranenburg
+ *
+ */
+public class SoapUI_TestCase extends TestCaseImpl
+{
+	private WsdlTestCase mySoapUITestCase;
+
+	public SoapUI_TestCase( WsdlTestCase aSoapUI_TestCase )
+	{
+		super( aSoapUI_TestCase.getName(),
+		       aSoapUI_TestCase.getDescription(),
+		       0,
+		       new ArrayList<String>(),
+		       new TestStepSequence(),
+		       new TestStepSequence(),
+		       new TestStepSequence(),
+		       new Hashtable<String, String>(),
+		       new Hashtable<String, String>() );
+		
+		mySoapUITestCase = aSoapUI_TestCase;
+	}
+
+	/**
+	 * @return the soapUI_TestCase
+	 */
+	public WsdlTestCase getSoapUI_TC()
+	{
+		return mySoapUITestCase;
+	}
+}
